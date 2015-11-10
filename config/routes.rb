594935a9bc71dc43d4117152852cb495ae138f8e
Julio Cesar Fausto <jcfausto@gmail.com>
2015-11-10 19:10:56 -0200

    Enabled to change the admin password when logged in. Also in the app/views/devise/registrations/edit.html.erb was necessary to apply a workarround described here (https://github.com/plataformatec/devise/issues/2667) in order to not raise and UnknownFormat error

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
   resources :achievements
   resources :projects
   resources :skills
+
   devise_for :admins, :skip => :registrations
+
+  #Enabling to change password even telling to skip registrations
+  as :admin do
+    get 'admins/edit' => 'devise/registrations#edit', :as => 'edit_admin_registration'
+    patch 'admins' => 'devise/registrations#update', :as => 'admin_registration'
+  end
+
   resources :hero_phrases
   resources :hero_phrases
   resources :abouts
