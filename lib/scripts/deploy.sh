aef4b4b9a524b46d8434eb2e4a21cde4fc021162
jcfausto <jcfausto@gmail.com>
2017-02-03 22:02:25 +0000

    Adding deployment scripts

---
+++ lib/scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+    # Initialize a new git repo and push it to our server.
+    git init
+        
+    git remote add deploy "deploye@jcfausto-com:/home/rails/jcfausto-rails-website"
+    git config user.name "Travis CI"
+    git config user.email "jcfausto@gmail.com"
+    
+    git add .
+    git commit -m "Deploy"
+    git push --force deploy master
+else
+    echo "Not deploying, since this branch isn't master."
+fi
