b67ac6bead6d4238f4f48af53630c1794ef8d765
jcfausto <jcfausto@gmail.com>
2017-02-04 19:02:10 +0000

    Fixing a typo

--- .unicorn.sh
+++ .unicorn.sh
@@ -8,10 +8,9 @@ export CONFIGURED=yes
 export TIMEOUT=50
 export APP_ROOT=/home/rails/www/jcfausto-rails-website/current
 export RAILS_ENV="production"
-export GEM_HOME="/home/rails/www/jcfausto-rails-website/curent/vendor/bundle"
-export GEM_PATH="/home/rails/www/jcfausto-rails-website/curent/vendor/bundle:/usr/local/rvm/gems/${ruby_ver}:/usr/local/rvm/gems/${ruby_ver}@global"
-export PATH="/home/rails/www/jcfausto-rails-website/curent/vendor/bundle/bin:/usr/local/rvm/gems/${ruby_ver}/bin:${PATH}"
+export GEM_PATH="/home/rails/www/jcfausto-rails-website/current/vendor/bundle:/usr/local/rvm/gems/${ruby_ver}:/usr/local/rvm/gems/${ruby_ver}@global"
+export PATH="/home/rails/www/jcfausto-rails-website/current/vendor/bundle/bin:/usr/local/rvm/gems/${ruby_ver}/bin:${PATH}"
 
 # Execute the unicorn process
-/home/rails/www/jcfausto-rails-website/curent/vendor/bundle/bin/unicorn \
+/home/rails/www/jcfausto-rails-website/current/vendor/bundle/bin/unicorn \
         -c /etc/unicorn.conf -E production --debug
